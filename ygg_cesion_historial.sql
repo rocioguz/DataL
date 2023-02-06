7715d0fc21e58c09b6e7e503861759b698cbe8d5
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-05 23:41:18 -0300

    Update ygg_cesion_historial.sql

--- ygg_cesion_historial.sql
+++ ygg_cesion_historial.sql
@@ -1,3 +1,7 @@
+CREATE TABLE "prod-dynamo"."ygg_cesion_historial" WITH (
+  format = 'parquet',
+  external_location = 's3://destination-athena-results/ygg_alta_legal_solicitud/'
+) AS
 SELECT 
     Item.empRut.N as emp_rut,
     Item.timestamp.N as time_stamp,
