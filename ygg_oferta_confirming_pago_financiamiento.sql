1632d4ee408e020baf517770415d0f9a618b3b77
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-05 23:59:13 -0300

    Update ygg_oferta_confirming_pago_financiamiento.sql

--- ygg_oferta_confirming_pago_financiamiento.sql
+++ ygg_oferta_confirming_pago_financiamiento.sql
@@ -1,3 +1,7 @@
+CREATE TABLE "prod-dynamo"."ygg_oferta_confirming_pago_financiamiento" WITH (
+  format = 'parquet',
+  external_location = 's3://destination-athena-results/ygg_oferta_confirming_pago_financiamiento/'
+) AS
 SELECT 
     Item.empRut.N as emp_rut,
     Item.emisorRut_folio_tipoDte.S as emisor_rut_folio_tipo_dte,
