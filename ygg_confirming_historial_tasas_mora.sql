29a02455287549b9f5921e33b5760580fd08028b
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-05 23:46:01 -0300

    Update ygg_confirming_historial_tasas_mora.sql

--- ygg_confirming_historial_tasas_mora.sql
+++ ygg_confirming_historial_tasas_mora.sql
@@ -1,3 +1,7 @@
+CREATE TABLE "prod-dynamo"."ygg_confirming_historial_tasas_mora" WITH (
+  format = 'parquet',
+  external_location = 's3://destination-athena-results/ygg_confirming_historial_tasas_mora/'
+) AS
 SELECT
     Item.empRut.N as emp_rut,
     CAST(From_iso8601_timestamp(Item.fechaCreacion.S) AS timestamp) as fecha_creacion,
