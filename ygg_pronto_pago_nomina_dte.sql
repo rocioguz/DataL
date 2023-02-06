c4320a4e9c0cdbaf01dbeb71937feafe567bdd50
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-06 00:04:56 -0300

    Update ygg_pronto_pago_nomina_dte.sql

--- ygg_pronto_pago_nomina_dte.sql
+++ ygg_pronto_pago_nomina_dte.sql
@@ -1,3 +1,7 @@
+CREATE TABLE "prod-dynamo"."ygg_pronto_pago_nomina_dte" WITH (
+  format = 'parquet',
+  external_location = 's3://destination-athena-results/ygg_pronto_pago_nomina_dte/'
+) AS
 SELECT 
     Item.empRut_timestamp.S as emp_rut_timestamp,
     Item.emisorRut_folio_tipoDte.S as emisor_rut_folio_tipo_dte,
