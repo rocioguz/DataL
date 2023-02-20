9c81864a5ff2e4743d20734526e40f0eb767ddd9
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-20 10:44:42 -0300

    Update ygg_confirming_historial_tasas_mora.sql

--- ygg_confirming_historial_tasas_mora.sql
+++ ygg_confirming_historial_tasas_mora.sql
@@ -1,10 +1,15 @@
 CREATE TABLE "prod-dynamo"."ygg_confirming_historial_tasas_mora" WITH (
   format = 'parquet',
-  external_location = 's3://destination-athena-results/ygg_confirming_historial_tasas_mora/'
+  external_location = 's3://staging.layer/DynamoDB/ygg_confirming_historial_tasas_mora/'
 ) AS
-SELECT
-    Item.empRut.N as emp_rut,
-    CAST(From_iso8601_timestamp(Item.fechaCreacion.S) AS timestamp) as fecha_creacion,
-    CAST(From_iso8601_timestamp(Item.fechaModificacion.S) AS timestamp) as fecha_modificacion,
-    Item.tasasMora.M.financiamientoDirecto.N as tasas_mora_financiamiento_directo
+SELECT CAST (Item.empRut.N AS int) as emp_rut,
+  CAST(
+    From_iso8601_timestamp(Item.fechaCreacion.S) AS timestamp
+  ) as fecha_creacion,
+  CAST(
+    From_iso8601_timestamp(Item.fechaModificacion.S) AS timestamp
+  ) as fecha_modificacion,
+  CAST (
+    Item.tasasMora.M.financiamientoDirecto.N AS double
+  ) as tasas_mora_financiamiento_directo
 FROM "prod-dynamo"."ddb_ygg_confirming_historial_tasas_mora";
