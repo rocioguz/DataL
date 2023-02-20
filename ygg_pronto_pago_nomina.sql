7334bb4228558de21aa0732bf31b37613a6b3997
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-20 14:03:43 -0300

    Update ygg_pronto_pago_nomina.sql

--- ygg_pronto_pago_nomina.sql
+++ ygg_pronto_pago_nomina.sql
@@ -1,13 +1,12 @@
 CREATE TABLE "prod-dynamo"."ygg_pronto_pago_nomina" WITH (
   format = 'parquet',
-  external_location = 's3://destination-athena-results/ygg_pronto_pago_nomina/'
+  external_location = 's3://staging.layer/DynamoDB/ddb_ygg_pronto_pago_nomina/'
 ) AS
-SELECT 
-    Item.empRut.N as emp_rut,
-    Item.timestamp.N as timestamp,
-    Item.cantidadDtes.N as cantidad_dtes,
-    Item.cantidadProveedores.N as cantidad_proveedores,
-    date_parse(Item.fechaActualizacion.S,'%Y-%m-%d %H:%i:%s') as fecha_actualizacion,
-    date_parse(Item.fechaCreacion.S,'%Y-%m-%d %H:%i:%s') as fecha_creacion,
-    Item.montoTotal.N as monto_total
+SELECT CAST (Item.empRut.N as int) as emp_rut,
+  Item.timestamp.N as timestamp,
+  CAST (Item.cantidadDtes.N as int) as cantidad_dtes,
+  CAST (Item.cantidadProveedores.N as int) as cantidad_proveedores,
+  date_parse(Item.fechaActualizacion.S, '%Y-%m-%d %H:%i:%s') as fecha_actualizacion,
+  date_parse(Item.fechaCreacion.S, '%Y-%m-%d %H:%i:%s') as fecha_creacion,
+  CAST (Item.montoTotal.N as bigint) as monto_total
 FROM "prod-dynamo"."ddb_ygg_pronto_pago_nomina";
