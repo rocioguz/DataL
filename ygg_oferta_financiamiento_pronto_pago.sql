2d6facb56b4c855df6cce6c9c35746153c3dd740
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-20 13:58:40 -0300

    Update ygg_oferta_financiamiento_pronto_pago.sql

--- ygg_oferta_financiamiento_pronto_pago.sql
+++ ygg_oferta_financiamiento_pronto_pago.sql
@@ -1,12 +1,11 @@
 CREATE TABLE "prod-dynamo"."ygg_oferta_financiamiento_pronto_pago" WITH (
   format = 'parquet',
-  external_location = 's3://destination-athena-results/ygg_oferta_financiamiento_pronto_pago/'
+  external_location = 's3://staging.layer/DynamoDB/ygg_oferta_financiamiento_pronto_pago/'
 ) AS
-SELECT 
-    Item.empRut.N as emp_rut,
-    Item.emisorRut_folio_tipoDte.S as emisor_rut_folio_tipo_dte,
-    Item.disponible.BOOL as disponible,
-    Item.empRut_disponible.S as emp_rut_disponible,
-    Item.empRut_disponible_receptorRut.S as emp_rut_disponible_receptor_rut,
-    Item.rutaXml.S as ruta_xml
+SELECT CAST (Item.empRut.N as int) as emp_rut,
+  Item.emisorRut_folio_tipoDte.S as emisor_rut_folio_tipo_dte,
+  CAST (Item.disponible.BOOL as boolean) as disponible,
+  Item.empRut_disponible.S as emp_rut_disponible,
+  Item.empRut_disponible_receptorRut.S as emp_rut_disponible_receptor_rut,
+  Item.rutaXml.S as ruta_xml
 FROM "prod-dynamo"."ddb_ygg_oferta_financiamiento_pronto_pago";
