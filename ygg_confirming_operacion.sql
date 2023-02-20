2709698c5e0c5d9cc2ea835e10c6391b7f781d3f
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-20 11:28:43 -0300

    Update ygg_confirming_operacion.sql

--- ygg_confirming_operacion.sql
+++ ygg_confirming_operacion.sql
@@ -1,49 +1,58 @@
 CREATE TABLE "prod-dynamo"."ygg_confirming_operacion" WITH (
   format = 'parquet',
-  external_location = 's3://destination-athena-results/ygg_confirming_operacion/'
+  external_location = 's3://staging.layer/DynamoDB/ygg_confirming_operacion/'
 ) AS
-SELECT 
-    Item.empRut.N as emp_rut,
-    Item.pgcId.N as pgc_id,
-    Item.comprobante.L as comprobante,
-    Item.detalle.M.clienteRazonSocial.S as detalle_cliente_razon_social,
-    Item.detalle.M.clienteRut.N as detalle_cliente_rut,
-    Item.detalle.M.estado.M.id.N as detalle_estado_id,
-    Item.detalle.M.estado.M.nombre.S as detalle_estado_nombre,
-    CAST(From_iso8601_timestamp(Item.detalle.M.fechas.M.curso.S) AS timestamp) as detalle_fechas_curso,
-    CAST(From_iso8601_timestamp(Item.detalle.M.fechas.M.ingreso.S) AS timestamp) as detalle_fechas_ingreso,
-    CAST(From_iso8601_timestamp(Item.detalle.M.fechas.M.termino.S) AS timestamp) as detalle_fechas_termino,
-    Item.detalle.M.montos.M.anticipo.N as detalle_montos_anticipo,
-    Item.detalle.M.montos.M.aplicaciones.N as detalle_montos_aplicaciones,
-    Item.detalle.M.montos.M.comision.N as detalle_montos_comision,
-    Item.detalle.M.montos.M.diferenciaPrecio.N as detalle_montos_diferencia_precio,
-    Item.detalle.M.montos.M.excedentes.N as detalle_montos_excedentes,
-    Item.detalle.M.montos.M.gestionLegalAdministrativa.N as detalle_montos_gestion_legal_administrativa,
-    Item.detalle.M.montos.M.giro.N as detalle_montos_giro,
-    Item.detalle.M.montos.M.iva.N as detalle_montos_iva,
-    Item.detalle.M.montos.M.mora.N as detalle_montos_mora,
-    Item.detalle.M.montos.M.total.N as detalle_montos_total,
-    Item.detalle.M.saldoCredito.N as detalle_saldo_credito,
-    Item.detalle.M.subProducto.M.id.N as detalle_sub_producto_credito_id,
-    Item.detalle.M.subProducto.M.nombre.S as detalle_sub_producto_credito_nombre,
-    Item.dtes.L as dtes, 
-    Item.empRut_fechaSolicitud.S as emp_rut_fecha_solicitud,
-    Item.empRut_fechaSolicitud_estado.S as emp_rut_fecha_solicitud_estado,
-    Item.empRut_tipoConfirming.S as emp_rut_tipo_confirming,
-    Item.empRut_tipoConfirming_estado.S as emp_rut_tipo_confirming_estado,
-    Item.estadoPago.M.idCxCConfirming.N as estado_pago_id_CxC_Confirming,
-    Item.estadoPago.M.monto.N as estado_pago_monto,
-    Item.estadoPago.M.montoPendiente.N as estado_pago_monto_pendiente,
-    Item.estadoPago.M.montoUtilizado.N as estado_pago_monto_utilizado,
-    Item.estadoPago.M.rut.N as estado_pago_rut,
-    Item.fechaActualizacion.S as fecha_actualizacion,
-    Item.fechaSolicitud.S as fecha_solicitud,
-    --From_iso8601_timestamp(Item.fechaActualizacion.S) as t1,
-    --CAST(From_iso8601_timestamp(Item.fechaActualizacion.S) AS timestamp) as fecha_actualizacion,
-    --CAST(From_iso8601_timestamp(Item.fechaSolicitud.S) AS timestamp) as fecha_solicitud,
-    Item.firmantes.L as firmantes, --falta desglosar
-    Item.rutaArchivo.S as ruta_archivo,
-    Item.simId.N as sim_id,
-    Item.subOperaciones.M.datos.S as sub_operaciones_datos, --falta desglosar
-    Item.tipoConfirming.S as tipo_confirming
+SELECT CAST (Item.empRut.N as int) as emp_rut,
+  CAST (Item.pgcId.N as int) as pgc_id,
+  Item.comprobante.L as comprobante,
+  Item.detalle.M.clienteRazonSocial.S as detalle_cliente_razon_social,
+  CAST (Item.detalle.M.clienteRut.N as int) as detalle_cliente_rut,
+  CAST (Item.detalle.M.estado.M.id.N as int) as detalle_estado_id,
+  Item.detalle.M.estado.M.nombre.S as detalle_estado_nombre,
+  CAST(
+    From_iso8601_timestamp(Item.detalle.M.fechas.M.curso.S) AS timestamp
+  ) as detalle_fechas_curso,
+  CAST(
+    From_iso8601_timestamp(Item.detalle.M.fechas.M.ingreso.S) AS timestamp
+  ) as detalle_fechas_ingreso,
+  CAST(
+    From_iso8601_timestamp(Item.detalle.M.fechas.M.termino.S) AS timestamp
+  ) as detalle_fechas_termino,
+  CAST (Item.detalle.M.montos.M.anticipo.N as int) as detalle_montos_anticipo,
+  CAST (Item.detalle.M.montos.M.aplicaciones.N as int) as detalle_montos_aplicaciones,
+  CAST (Item.detalle.M.montos.M.comision.N as int) as detalle_montos_comision,
+  CAST (
+    Item.detalle.M.montos.M.diferenciaPrecio.N as int
+  ) as detalle_montos_diferencia_precio,
+  CAST (Item.detalle.M.montos.M.excedentes.N as int) as detalle_montos_excedentes,
+  CAST (
+    Item.detalle.M.montos.M.gestionLegalAdministrativa.N as int
+  ) as detalle_montos_gestion_legal_administrativa,
+  CAST (Item.detalle.M.montos.M.giro.N as int) as detalle_montos_giro,
+  CAST (Item.detalle.M.montos.M.iva.N as int) as detalle_montos_iva,
+  CAST (Item.detalle.M.montos.M.mora.N as int) as detalle_montos_mora,
+  CAST (Item.detalle.M.montos.M.total.N as int) as detalle_montos_total,
+  CAST (Item.detalle.M.saldoCredito.N as int) as detalle_saldo_credito,
+  CAST (Item.detalle.M.subProducto.M.id.N as int) as detalle_sub_producto_credito_id,
+  Item.detalle.M.subProducto.M.nombre.S as detalle_sub_producto_credito_nombre,
+  Item.dtes.L as dtes,
+  Item.empRut_fechaSolicitud.S as emp_rut_fecha_solicitud,
+  Item.empRut_fechaSolicitud_estado.S as emp_rut_fecha_solicitud_estado,
+  Item.empRut_tipoConfirming.S as emp_rut_tipo_confirming,
+  Item.empRut_tipoConfirming_estado.S as emp_rut_tipo_confirming_estado,
+  CAST (Item.estadoPago.M.idCxCConfirming.N as int) as estado_pago_id_CxC_Confirming,
+  CAST (Item.estadoPago.M.monto.N as int) as estado_pago_monto,
+  CAST (Item.estadoPago.M.montoPendiente.N as int) as estado_pago_monto_pendiente,
+  CAST (Item.estadoPago.M.montoUtilizado.N as int) as estado_pago_monto_utilizado,
+  CAST (Item.estadoPago.M.rut.N as int) as estado_pago_rut,
+  Item.fechaActualizacion.S as fecha_actualizacion,
+  Item.fechaSolicitud.S as fecha_solicitud,
+  --From_iso8601_timestamp(Item.fechaActualizacion.S) as t1,
+  --CAST(From_iso8601_timestamp(Item.fechaActualizacion.S) AS timestamp) as fecha_actualizacion,
+  --CAST(From_iso8601_timestamp(Item.fechaSolicitud.S) AS timestamp) as fecha_solicitud,
+  Item.firmantes.L as firmantes,  --falta desglosar
+  Item.rutaArchivo.S as ruta_archivo,
+  CAST (Item.simId.N as int) as sim_id,
+  Item.subOperaciones.M.datos.S as sub_operaciones_datos,  --falta desglosar
+  Item.tipoConfirming.S as tipo_confirming
 FROM "prod-dynamo"."ddb_ygg_confirming_operacion";
