57a6f93daf28396ab686a2f1eab23d18ab967366
rocioguz <67564587+rocioguz@users.noreply.github.com>
2023-02-21 23:25:36 -0300

    Update ygg_usuarios.sql

--- ygg_usuarios.sql
+++ ygg_usuarios.sql
@@ -4,14 +4,14 @@ CREATE TABLE "prod-dynamo"."ygg_usuarios" WITH (
 ) AS
 SELECT 
     Item.correo.S as correo,
-    Item.activo.N as activo,
+    CAST (Item.activo.N as int) as activo,
     Item.apellidos.S as apellidos,
     Item.cognitoId.S as cognito_id,
     Item.empresas.NS as empresas,
     CAST(From_iso8601_timestamp(Item.fechaCreacion.S) AS timestamp) as fecha_creacion,
     CAST(From_iso8601_timestamp(Item.fechaModificacion.S) AS timestamp) as fecha_modificacion,
     Item.nombres.S as nombres,
-    Item.soporte.BOOL as soporte,
+    CAST (Item.soporte.BOOL as boolean) as soporte,
     Item.telefono.S as telefono,
     CAST(From_iso8601_timestamp(Item.tyc.M.fechaAceptacion.S ) AS timestamp) as tyc_fecha_aceptacion,
     CAST(From_iso8601_timestamp(Item.tyc.M.fechaModificacion.S) AS timestamp) as tyc_fecha_modificacion,
